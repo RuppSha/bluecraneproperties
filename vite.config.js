fb683b39c09afafe3a03ba3db1049646784f8e05
RuppSha <sharline.rupp@gmail.com>
2025-06-05 11:58:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/apps/dealanalyzer/', // <-- This is the key line!
   plugins: [react()],
-  server: {
-    port: 3000
-  }
-});
+})
